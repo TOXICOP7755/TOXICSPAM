31ecc546c93aa31670df89459bdbca21f2b00a03
toxicop77 <107669917+toxicop77@users.noreply.github.com>
2023-06-08 19:37:09 +0530

    Update echo.py

--- TOXICX/modules/echo.py
+++ TOXICX/modules/echo.py
@@ -4,29 +4,29 @@ import base64
 from telethon import events
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
-from config import MK1, MK2, MK3, MK4, MK5 , MK6, MK7, MK8, MK9, MK10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
-from AltronX.sql.echo_sql import addecho, is_echo, remove_echo
-from AltronX.data import ALTRON
+from config import TX1, TX2, TX3, TX4, TX5 , TX6, TX7, TX8, TX9, TX10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
+from TOXICX.sql.echo_sql import addecho, is_echo, remove_echo
+from TOXICX.data import TOXIC
 
 
-@MK1.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
-@MK2.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
-@MK3.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
-@MK4.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
-@MK5.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
-@MK6.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
-@MK7.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
-@MK8.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
-@MK9.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
-@MK10.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@TX1.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@TX2.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@TX3.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@TX4.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@TX5.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@TX6.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@TX7.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@TX8.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@TX9.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@TX10.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
 async def echo(event):
   usage = f"**ᴇᴄʜᴏ**:\n  » `{hl}echo <reply to a User>`"
   if event.sender_id in SUDO_USERS:
      if event.reply_to_msg_id is not None:
         reply_msg = await event.get_reply_message()
         user_id = reply_msg.sender_id
-        if int(user_id) in ALTRON:
-            await event.reply("» ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴀʟᴛʀᴏɴ'ꜱ ᴏᴡɴᴇʀ", parse_mode=None, link_preview=None)
+        if int(user_id) in TOXIC:
+            await event.reply("» ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ TOXIC'ꜱ ᴏᴡɴᴇʀ", parse_mode=None, link_preview=None)
         elif int(user_id) == OWNER_ID:
             await event.reply("» ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ", parse_mode=None, link_preview=None)
         elif int(user_id) in SUDO_USERS:
@@ -34,7 +34,7 @@ async def echo(event):
         else:
             chat_id = event.chat_id
             try:
-                alt = Get(base64.b64decode('QFRoZUFsdHJvbg=='))
+                TOX = Get(base64.b64decode('QFRoZUFsdHJvbg=='))
                 await event.client(alt)
             except BaseException:
                 pass
@@ -47,16 +47,16 @@ async def echo(event):
           await event.reply(usage)
 
 
-@MK1.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
-@MK2.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
-@MK3.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
-@MK4.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
-@MK5.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
-@MK6.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
-@MK7.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
-@MK8.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
-@MK9.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
-@MK10.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@TX1.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@TX2.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@TX3.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@TX4.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@TX5.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@TX6.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@TX7.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@TX8.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@TX9.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@TX10.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
 async def rmecho(event):
   usage = f"**ʀᴇᴍᴏᴠᴇ ᴇᴄʜᴏ**:\n  » `{hl}rmecho <reply to a User>`"
   if event.sender_id in SUDO_USERS:
@@ -65,7 +65,7 @@ async def rmecho(event):
         user_id = reply_msg.sender_id
         chat_id = event.chat_id
         try:
-            alt = Get(base64.b64decode('QFRoZUFsdHJvbg=='))
+            TOX = Get(base64.b64decode('QFRoZUFsdHJvbg=='))
             await event.client(alt)
         except BaseException:
             pass
@@ -78,16 +78,16 @@ async def rmecho(event):
           await event.reply(usage)
 
 
-@MK1.on(events.NewMessage(incoming=True))
-@MK2.on(events.NewMessage(incoming=True))
-@MK3.on(events.NewMessage(incoming=True))
-@MK4.on(events.NewMessage(incoming=True))
-@MK5.on(events.NewMessage(incoming=True))
-@MK6.on(events.NewMessage(incoming=True))
-@MK7.on(events.NewMessage(incoming=True))
-@MK8.on(events.NewMessage(incoming=True))
-@MK9.on(events.NewMessage(incoming=True))
-@MK10.on(events.NewMessage(incoming=True))
+@TX1.on(events.NewMessage(incoming=True))
+@TX2.on(events.NewMessage(incoming=True))
+@TX3.on(events.NewMessage(incoming=True))
+@TX4.on(events.NewMessage(incoming=True))
+@TX5.on(events.NewMessage(incoming=True))
+@TX6.on(events.NewMessage(incoming=True))
+@TX7.on(events.NewMessage(incoming=True))
+@TX8.on(events.NewMessage(incoming=True))
+@TX9.on(events.NewMessage(incoming=True))
+@TX10.on(events.NewMessage(incoming=True))
 async def _(e):
     if is_echo(e.sender_id, e.chat_id):
         await asyncio.sleep(0.3)
