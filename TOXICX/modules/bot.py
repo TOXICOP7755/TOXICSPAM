cfd571525ecf5fab2f98ab6ea4c73179e8246013
toxicop77 <107669917+toxicop77@users.noreply.github.com>
2023-06-08 19:41:58 +0530

    Update bot.py

--- TOXICX/modules/bot.py
+++ TOXICX/modules/bot.py
@@ -120,7 +120,7 @@ async def addsudo(event):
         else:
             newsudo = f"{target}"
         await ok.edit(f"» **ɴᴇᴡ ꜱᴜᴅᴏ ᴜꜱᴇʀ**: `{target}`\n» `ʀᴇsᴛᴀʀᴛɪɴɢ ʙᴏᴛ...`")
-        heroku_var[TXs] = newsudo   
+        heroku_var[TXS] = newsudo   
    
      
 async def get_user(event):
